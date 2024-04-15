cc3dabd7a8abf90da1f443edcc055a0e64148467
unknown <prmalakoti@gmail.com>
2024-04-15 16:11:41 +0530

    streaming, express monitor and docker updated

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Api Rate Limit for ExpressJS
 ## npm start
 
 ### It will apply api rateLimit whole app || we can set specific api as well both example mentioned in the app.ja file
+
+### monitor the express app
+
+- -npm i express-status-monitor
+
+#### import and use in middleware as a method
+
+### one new topic streaming the file it means read file chunk by chunk and send to browser - it will take less memory
+
+### normal fs.read file it will take more memory, In production maybe it will crash the server due to insufficent memory.
--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
+const status = require("express-status-monitor");
+let fs = require('fs');
 
 const app = express();
 
 const limiter = rateLimit({
     max: 5,
-    windowMs: 1 * 60 * 1000, // 1 minute start time consifder first api throshold crossed
+    windowMs: 1 * 60 * 1000, // 1 minute start time consider first api throshold crossed
     message: "Too many request from this IP",
     Headers: true
 });
 
-app.use(limiter); // It will apply the aoi rate limit whole server 
+app.use(status()); // monitor the app status
+app.use(limiter); // It will apply the rate limit whole server 
 
 // GET route to handle the request coming from user
 app.get("/", (req, res) => {
@@ -25,6 +28,20 @@ app.get("/route", limiter, (req, res) => {
     res.status(200).json({
         status: "success",
         message: "Hello from the Developer express server with specific api"
+    });
+});
+
+/* stream the file */
+app.get("/stream", (req, res) => {
+    let stream = fs.createReadStream('data.json');
+    stream.on("data", (chunk) => res.write(chunk));
+    stream.on("end", () => res.end());
+});
+
+/* it will consume the more memory */
+app.get("/non-stream", (req, res) => {
+    fs.readFile('data.json', 'utf-8', (err, data) => {
+        res.send(data)
     });
 });
 
